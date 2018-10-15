7edea625192d430284e40adb5407404f3df531b5
Matthew Peixoto <matthewpeixoto@gmail.com>
2018-10-15 16:27:46 -0400

    Fix modal scroll on iOS

--- js/modal.js
+++ js/modal.js
@@ -4,30 +4,19 @@ var $html = $('html');
 var $body = $('body');
 var $modal = $('.modal');
 
-// $posterBtn.click(function () {
-//   var offset = $(document).scrollTop();
-//   var viewportHeight = $(window).height();
-//   $modal.css('top', (offset  + (viewportHeight/2)) - ($modal.outerHeight()/2));
-//   $modal.show();
-//   $html.add($body).css({
-//     overflow: 'hidden',
-//     position: 'relative'
-//   });
-// });
-
-// $btnCloseModal.click(function () {
-//   $modal.hide();
-//   $html.add($body).css({
-//     overflow: '',
-//     position: ''
-//   });
-// })
-
 $posterBtn.on('click', function () {
   $(this).next().removeClass('hide');
+  $html.add($body).css({
+    overflow: 'hidden',
+    position: 'relative'
+  });
 });
 
 $btnCloseModal.on('click', function () {
   $modal.addClass('hide');
+  $html.add($body).css({
+    overflow: '',
+    position: ''
+  });
 });
 
