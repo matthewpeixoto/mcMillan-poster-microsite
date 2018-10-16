eb547117437802e2d4b3fe5986c8811241ee6a37
Matthew Peixoto <matthewpeixoto@gmail.com>
2018-10-16 11:49:45 -0400

    Add functional menu

--- js/header.js
+++ js/header.js
@@ -1,6 +1,16 @@
-var $btnToggle = $('.btn-toggle');
-var $navXS = $('.nav-xs');
+var $toggleMenu = $('#toggle-menu');
+var $body = $('.body');
 
-$btnToggle.click(function () {
-  $navXS.slideToggle(400);
+$(document).ready(function(){
+
+  $toggleMenu.click(function () {
+    $('.menu-container').slideToggle(550);
+    $('.menu-item').toggleClass('js-menu-item-active');
+    $('.hamburger').toggleClass('is-active');
+    $('.logo').toggleClass('js-logo-active');
+    $html.toggleClass('overflow-hidden');
+    $body.toggleClass('overflow-hidden');
+  });
+
 });
+
