dfadcf161796bf5b14511c848325bc3b215c8266
Matthew Peixoto <matthewpeixoto@gmail.com>
2018-12-20 10:45:55 -0500

    Fix scroll

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,13 @@
 var $posterBtn = $('.poster-btn')
 var $btnCloseModal = $('.btn-close-modal');
-var $html = $('html');
-var $body = $('body');
 var $modal = $('.modal');
 
 $posterBtn.on('click', function () {
   $(this).next().addClass('js-open-modal');
-  $body.addClass("modal-open");
+  $('body').addClass("modal-open");
 });
 
 $btnCloseModal.on('click', function () {
   $modal.removeClass('js-open-modal');
-  $body.removeClass("modal-open");
+  $('body').removeClass("modal-open");
 });
-
