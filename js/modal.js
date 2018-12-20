7be47afbdb21d88dac9b2fc8752ee0b3aa12078e
Matthew Peixoto <matthewpeixoto@gmail.com>
2018-12-20 11:35:24 -0500

    Try iPhone specific target

--- js/modal.js
+++ js/modal.js
@@ -2,14 +2,17 @@ var $posterBtn = $('.poster-btn')
 var $btnCloseModal = $('.btn-close-modal');
 var $modal = $('.modal');
 
+var ua = navigator.userAgent;
+ua = ua.toString();
+$('html').attr('id', ua);
+
 $posterBtn.on('click', function () {
   $(this).next().addClass('js-open-modal');
-  $('html').addClass("modal-open");
-  $('body').addClass("modal-open");
+  $('html').addClass('modal-open');
+  e.preventDefault();
 });
 
 $btnCloseModal.on('click', function () {
   $modal.removeClass('js-open-modal');
   $('html').removeClass("modal-open");
-  $('body').removeClass("modal-open");
 });
