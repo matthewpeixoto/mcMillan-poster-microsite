b8c247d3abff60873f8650da7355aef6462f11ca
Matthew Peixoto <matthewpeixoto@gmail.com>
2019-01-04 13:35:37 -0500

    Add progress bar on load

--- js/main.js
+++ js/main.js
@@ -71,3 +71,26 @@ if(!isMobile) {
     sensitivity: 150
   });
 }
+
+// Progress
+
+$(function() {
+  var $progress = $('#progress');
+  $(document).ajaxStart(function() {
+    //only add progress bar if not added yet.
+    if ($progress.length === 0) {
+      $progress = $('<div><dt/><dd/></div>').attr('id', 'progress');
+      $("body").append($progress);
+    }
+    $progress.width((50 + Math.random() * 30) + "%");
+  });
+
+  $(document).ajaxComplete(function() {
+    //End loading animation
+    $progress.width("100%").delay(200).fadeOut(400, function() {
+      $progress.width("0%").delay(200).show();
+    });
+  });
+
+  $.getJSON('http://jsonip.com');
+});
